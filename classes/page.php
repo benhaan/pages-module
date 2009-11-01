81d7c1ea4f3b0990a0e000be7caab01e21d0ccb8
Ben Haan <ben@benhaan.com>
2009-10-31 23:18:17 -0400

    created pointer classes

---
+++ classes/page.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+* @package Pages Module
+*/
+class Page extends Kohana_Page {}
---
+++ classes/pages.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+* @package Pages Module
+* @author Ben Haan
+*/
+class Pages extends Kohana_Pages {}
